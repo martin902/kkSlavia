0f4090d3b4bd3cf775093f9b18c5d4bcd18f63a4
Martin <din.martin@gmail.com>
2021-02-20 17:01:42 +0100

    http://martin902.github.com/kkSlavia/public

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'http://localhost/';
+	public $baseURL = 'http://martin902.github.com/kkSlavia/public';
 
 	/**
 	 * --------------------------------------------------------------------------
